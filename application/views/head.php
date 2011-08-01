060cff0648e3f80bd8fe96024917aec63f138d70
Rick Burgess <public@rickburgess.me>
2011-08-01 21:21:03 +0100

    config

--- application/views/head.php
+++ application/views/head.php
@@ -3,5 +3,5 @@
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to Tweet Track</title>
-	<link type="text/css" rel="stylesheet" href="/tr/_css/core.css" media="all">
+	<link type="text/css" rel="stylesheet" href="/tweettrack/_css/core.css" media="all">
 </head>
