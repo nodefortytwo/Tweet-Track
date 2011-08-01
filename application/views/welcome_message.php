90c0b5ca484891366e388316e293e607edb6dcd2
Rick Burgess <public@rickburgess.me>
2011-08-01 21:27:51 +0100

    added to welcome message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,4 +1,7 @@
 
 <p>This site attempts to take daily snapshots of tweets matching search terms (any search that the twitter api supports) and provide some basic analytics.</p>
 <p>The site is in an extremely experimental stage and may still contain errors.</p>
+
+<p><strong>If you want any search tracked for future analytics Tweet me @nodefortytwo</strong></p>
+
 <p>Here are all the queries we are currently tracking</p>
