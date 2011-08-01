17dafcdce024e200b910328f08b3bd18a8f17d2f
Rick Burgess <public@rickburgess.me>
2011-08-01 21:19:31 +0100

    config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'localhost/tr';
+$config['base_url']	= 'rickburgess.me/tweettrack';
 
 /*
 |--------------------------------------------------------------------------
